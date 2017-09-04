5fb05d4e2fab565f1f7df4bff2bb6c4113633b07
Tihomir Car <tcar121293@gmail.com>
2017-09-04 21:40:24 +0200

    ready

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(routes)
 app.route('*').get((req,res)=>{
   res.sendFile(path.join(__dirname+'/client/public/index.html'))
 })
-const port = 5000
+const port = process.env.PORT||5000
 app.listen(port,()=>{
 console.log('app listening od port' + port)
 })
